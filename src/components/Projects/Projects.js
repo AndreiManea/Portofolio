f5b76a17fb559b5ddadde1a31576fec87f4ac832
andreimediatack <andrei.manea@mediatack.de>
2021-03-29 11:24:33 +0300

    Added different Resume, 2 new project and restyled stuff.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,19 @@ function Projects() {
       <h1 className="projects__title">PROJECTS</h1>
       <div className="container">
         <Project
+          title="Scientify"
+          image="Scientify"
+          github="https://github.com/adina-hub/MyExperiment/tree/develop"
+          website="https://myexperiment-c6404.web.app/"
+        />
+        <Project
+          title="Linkedin Clone"
+          image="LinkedinClone"
+          align="right"
+          github="https://github.com/AndreiManea/linkedin-clone"
+          website="https://linkedin-clone-4fb93.web.app/"
+        />
+        <Project
           title="Pizza Noastra"
           image="Pizza Noastra"
           github="https://github.com/AndreiManea/Pizza-Noastra"
@@ -26,6 +39,7 @@ function Projects() {
           github="https://github.com/AndreiManea/Spotify-Clone"
           website="https://spotify-clone-a04f0.web.app/"
         />
+
       </div>
     </div>
   );
